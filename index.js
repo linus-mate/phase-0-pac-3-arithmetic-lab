d49047bf591dc30dbceb8a582f640e8c66f34bea
linus mutugi mate <mlynux@gmail.com>
2022-08-03 19:28:06 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,32 @@
+function makeInt(n){
+    return parseInt(n,10)
+};
+function preserveDecimal(n){
+    return parseFloat(n)
+};
+function add(a, b){
+    return (a + b);
+};
+add(965, 1); 
+function subtract(a, b){
+    return (a - b);
+};
+subtract(10, 5);
+function multiply(a, b){
+    return (a * b);
+};
+multiply(10, 10);
+function divide(a, b){
+    return (a / b);
+};
+divide(20, 2);
+
+function increment(n){
+    n++;
+    return n;
+}
+
+function decrement(n){
+    n--;
+    return n;
+}
